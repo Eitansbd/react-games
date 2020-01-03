c8306c6d76c53c1c8d624e3926ef1ea2e848ceb5
Eitan Bar-David <eitansbd@gmail.com>
2020-01-03 17:00:52 +0000

    Updates

---
+++ precache-manifest.7d749aa2e77ec423baf6a54df897623a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f67ccbf536a1003d29695a61d727e12c",
+    "url": "/react-games/index.html"
+  },
+  {
+    "revision": "d23b1be6db781ee6313e",
+    "url": "/react-games/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "3f1c18bb0ab7ffe26ee0",
+    "url": "/react-games/static/css/main.cbc1e8b7.chunk.css"
+  },
+  {
+    "revision": "d23b1be6db781ee6313e",
+    "url": "/react-games/static/js/2.500a6e95.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-games/static/js/2.500a6e95.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3f1c18bb0ab7ffe26ee0",
+    "url": "/react-games/static/js/main.d63db9d8.chunk.js"
+  },
+  {
+    "revision": "d9298455f82895be89bf",
+    "url": "/react-games/static/js/runtime-main.cc749bc8.js"
+  }
+]);
